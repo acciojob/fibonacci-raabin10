004671098aa09c86b13d80ca4b02baf39c6cabf0
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2023-09-08 20:26:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+
 function fibonacci(num) {
-// your code here
+  if (num <= 0) {
+    return 0;
+  } else if (num === 1) {
+    return 0;
+  } else if (num === 2) {
+    return 1;
+  } else {
+    let fibList = [0, 1];
+    for (let i = 2; i < num; i++) {
+      let nextFib = fibList[i - 1] + fibList[i - 2];
+      fibList.push(nextFib);
+    }
+    return fibList[num - 1];
+  }
+}
 }
 
 module.exports = fibonacci;
